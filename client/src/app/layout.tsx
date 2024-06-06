9d810d055d73aaec58ef5842d05218fae78a8513
likhon29 <mdabdurrouf.likhon@gmail.com>
2024-06-07 03:39:50 +0600

    folder structure and others setup

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import Providers from "@/lib/Providers/Providers";
+import { Toaster } from "sonner";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -14,12 +17,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body>
+          <AppRouterCacheProvider>
+            <>
+              <Toaster position="top-center" />
+              {children}
+            </>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </Providers>
   );
 }
